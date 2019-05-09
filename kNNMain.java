13e3d8a3a10e91506f6a6fc85d330837543f9ea0
rtchinov <49948464+rtchinov@users.noreply.github.com>
2019-05-08 21:27:06 -0400

    Testing commit&push

--- kNNMain.java
+++ kNNMain.java
@@ -10,9 +10,10 @@ public class kNNMain{
 
     // TASK 1: Use command line arguments to point DataSet.readDataSet method to
     // the desired file. Choose a given DataPoint, and print its features and label
-     List<DataPoint> datasetNew = new ArrayList<DataPoint>();
-      datasetNew=DataSet.readDataSet; 
-
+    List<DataPoint> datasetNew = new ArrayList<DataPoint>();
+    datasetNew=DataSet.readDataSet; 
+    //Rachel Tchinov
+    
     //TASK 2:Use the DataSet class to split the fullDataSet into Training and Held Out Test Dataset
 
 
