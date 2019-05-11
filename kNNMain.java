89f86b8d178238ab4648e5b5eb55e9007cac8649
rtchinov <49948464+rtchinov@users.noreply.github.com>
2019-05-11 16:05:03 -0400

    TASK 6 partial 16:04pm 11/05/2019

--- kNNMain.java
+++ kNNMain.java
@@ -37,32 +37,62 @@ public static void main(String... args) throws FileNotFoundException{
 	// TASK 6: loop over the datapoints in the held out test set, and make predictions for Each
     // point based on nearest neighbors in training set. Calculate accuracy of model.
 	
-		
+	double [] meanArray = new double [1000];
+	double [] standardDevArray = new double [1000]; 
 	
+	for (int count=1; count<=1000; count ++)
+	{
+		
 			
-		    //TASK 2:Use the DataSet class to split the fullDataSet into Training and Held Out Test Dataset
-			List<DataPoint> trainingDataSet=DataSet.getTrainingSet(Data,fractionTrainingSet); 
-			List<DataPoint> testingDataSet=DataSet.getTestSet(Data,fractionTestSet); 
+		//TASK 2:Use the DataSet class to split the fullDataSet into Training and Held Out Test Dataset
+		List<DataPoint> trainingDataSet=DataSet.getTrainingSet(Data,fractionTrainingSet); 
+		List<DataPoint> testingDataSet=DataSet.getTestSet(Data,fractionTestSet); 
 			
-		    
 			
 		
+		// TASK 4: write a new method in DataSet.java which takes as arguments to DataPoint objects,
+	    // and returns the Euclidean distance between those two points (as a double)
+			
+		double [] array= new double[Data.size()]; //for task 6
+		for (int k=0; k<Data.size(); k++)
+		{
+				
+		// TASK 5: Use the KNNClassifier class to determine the k nearest neighbors to a given DataPoint,
+			// and make a print a predicted target label
+			//implementation of k-nearest-neighbor
+				
+			//prediction
+			
+			//put results into an array
+			
+				//array=      ;
+			
+			
+			
+			
 		
-		    // TASK 4: write a new method in DataSet.java which takes as arguments to DataPoint objects,
-		    // and returns the Euclidean distance between those two points (as a double)
-			for (int k=0; k<Data.size(); k++)
-			{
+			
+				
+	
 			System.out.println("Distance to point is " + DataSet.distanceEuclid(Data.get(k), dataPoint ));
 			}
-		    // TASK 5: Use the KNNClassifier class to determine the k nearest neighbors to a given DataPoint,
-		    // and make a print a predicted target label
-				
-		
-		    // TASK 6: loop over the datapoints in the held out test set, and make predictions for Each
-		    // point based on nearest neighbors in training set. Calculate accuracy of model.
 			
-  }
+			//accuracy 
+			
+			
+	System.out.println("The accuracy of the model: " + count ); 
+	System.out.println("The mean is:" + mean(array));
+	System.out.println("The standard Deviation is: " + standardDeviation(array));
+	meanArray[count]=mean(array); 
+	standardDevArray[count]=standardDeviation(array); 
+	}
+	System.out.println("THE ACCURACY OF THE OVERALL MODEL: "); 
+	System.out.println("The average accuracy is:" + mean(meanArray));
+	System.out.println("The standard Deviation of accuracy is: " + standardDeviation(standardDevArray));
 
+  }
+// TASK 6: loop over the datapoints in the held out test set, and make predictions for Each
+		    // point based on nearest neighbors in training set. Calculate accuracy of model.
   public static double mean(double[] arr){
     /*
     Method that takes as an argument an array of doubles
